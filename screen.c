edb3016cf63c4c2b6892955c03cf9ce5f0c71a41
eugenefischer <66030419+eugenefischer@users.noreply.github.com>
2020-10-27 23:47:43 -0500

    bugfix

--- screen.c
+++ screen.c
@@ -9,8 +9,8 @@ int sorientation = ROTATE_0;
 
 //putting in variables that should come from config.h
 //I don't know why they aren't showing up during compile
-int rows = 16;
-int cols =7;
+unsigned int rows = 16;
+unsigned int cols =7;
 unsigned int screen_type = WAVESHARE2IN7;
 
 
